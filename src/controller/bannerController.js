5476a10060141e7a9313dbcf5bf358ce62f408c1
Hasan Masud <hmmasud635@gmail.com>
2025-10-19 08:30:52 +0600

    banner add

--- src/controller/bannerController.js
+++ src/controller/bannerController.js
@@ -1,10 +1,54 @@
-// banner added
-const bannerController = async (req, res, next) => {
-  console.log(req.body);
-  
-  res.send(req.file)
+const bannerModel = require("../model/banner.model");
 
+// banner added
+const bannerController = async (req, res) => {
+  let { link } = req.body
+  let { filename } = req.file
+
+  try {
+    let banner = await new bannerModel({
+      image: `${process.env.SERVER_URL}/${filename}`,
+      link
+    })
+    await banner.save()
+    return res
+      .status(201)
+      .json({
+        success: true,
+        message: "Banner created successfull",
+        data: banner
+
+      })
+
+  } catch (error) {
+    return res
+      .status(500)
+      .json({
+        success: false,
+        message: error.message || error
+      })
+  }
+}
+
+const deleteBannerController = async (req, res) => {
+  try {
+    let { id } = req.params
+   await bannerModel.findByIdAndDelete(id)
+    return res
+      .status(200)
+      .json({
+        success: true,
+        message: "Banner delete successfull"
+      })
+  } catch (error) {
+    return res
+      .status(500)
+      .json({
+        success: false,
+        message: error.message || error
+      })
+  }
 }
 
 
-module.exports = { bannerController }
+module.exports = { bannerController, deleteBannerController }
--- src/route/api/banner/index.js
+++ src/route/api/banner/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const multer  = require('multer')
-const { bannerController } = require("../../../controller/bannerController");
-
+const multer = require('multer')
+const {
+  bannerController,
+  deleteBannerController
+} = require("../../../controller/bannerController");
 const router = express.Router()
+const path = require("path");
+const { tokenCheckMiddelware, adminCheck } = require("../../../utils/authMiddelware");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -11,13 +15,32 @@ const storage = multer.diskStorage({
   filename: function (req, file, cb) {
     const rendomtext = Date.now() + '-' + Math.round(Math.random() * 1E9)
     let fileextanstion = file.originalname.split(".")
-    let extanstion = fileextanstion[fileextanstion.length-1]
+    let extanstion = fileextanstion[fileextanstion.length - 1]
     cb(null, file.fieldname + '-' + rendomtext + "." + extanstion)
   }
 })
 
-const upload = multer({ storage: storage })
+function checkFileType(file, cb) {
+  const filetypes = /jpeg|jpg|png|PNG|gif|mp4|wmv/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
 
-router.post("/addbanner", upload.single("bannerimg"), bannerController)
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb('Error: Images only! (jpeg, jpg, png, PNG, gif, mp4, wmv)');
+  }
+}
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 2097152 },
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  }
+}) //2MB
+
+router.post("/addbanner", upload.single("bannerimg"), tokenCheckMiddelware, adminCheck, bannerController)
+router.delete("/deletebanner/:id", tokenCheckMiddelware, adminCheck, deleteBannerController)
 
 module.exports = router
